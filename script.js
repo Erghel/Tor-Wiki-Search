8b94cdf109224e7c4f802c844e98d8aa7cad6aa7
Erghel <mysterium1337@gmail.com>
2022-06-15 19:09:11 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function TorWiki() {
+    const tr = document.getElementById("search__input").value; 
+    const newURL = `https://duckduckgo.com/?q=${tr}+&sites=support.torproject.org&atb=v322-1&ia=web`;
+
+
+    window.open(newURL, "blank_");
+}
+
+document.getElementById("poisk").addEventListener('click', TorWiki)
