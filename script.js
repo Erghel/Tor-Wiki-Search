93c54504b534b03c2d67dc4ed24a95f5f27c4c1f
Erghel <mysterium1337@gmail.com>
2022-06-15 19:10:14 +0300

    fix tabs

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ function TorWiki() {
     const tr = document.getElementById("search__input").value; 
     const newURL = `https://duckduckgo.com/?q=${tr}+&sites=support.torproject.org&atb=v322-1&ia=web`;
 
-
     window.open(newURL, "blank_");
 }
 
